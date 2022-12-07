296081ff57b0f6308d2140290d3eecc8b323e271
Jeslin-C-John <111417450+Jeslin-C-John@users.noreply.github.com>
2022-12-07 20:18:08 +0530

    AddController

--- TaskLogger/Controllers/AddController.cs
+++ TaskLogger/Controllers/AddController.cs
@@ -1,19 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using TaskLogger.Models;
 
 namespace TaskLogger.Controllers
 {
     public class AddController : Controller
     {
-        // GET: Add
+        [HttpGet]
         public ActionResult Index()
         {
-
-
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Task instance)
+        {
+            Task taskobj = new Task();
+            using (SqlConnection con = new SqlConnection("Data Source=DESKTOP-28UGTAO;Initial Catalog=TaskLogger;Integrated Security=True"))
+            {
+                using (SqlCommand cmd = new SqlCommand("addtask", con))
+                {
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    //cmd.Parameters.AddWithValue("@name", instance.Name);
+                    //cmd.Parameters.AddWithValue("@email", instance.Email);
+                    //cmd.Parameters.AddWithValue("@password", instance.EncryptPass);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                }
+
+            }
+            return RedirectToAction("Index", "Login", new { area = "" });
         }
     }
 }
