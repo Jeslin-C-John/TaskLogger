7df3e9b70d638dafeb2fe0e8acae78857b8b1138
Jeslin-C-John <cjohnjeslin@gmail.com>
2022-12-27 12:37:58 +0530

    Delete popup completed

--- TaskLogger/Controllers/ViewController.cs
+++ TaskLogger/Controllers/ViewController.cs
@@ -78,5 +78,32 @@ namespace TaskLogger.Controllers
 
         }
 
+        public ActionResult Delete(Task instance, int Taskid)
+
+        {
+            ViewBag.Name = Session["Name"];
+            if (ModelState.IsValid)
+            {
+
+                using (SqlConnection con = new SqlConnection("Data Source=HP_5300U;Initial Catalog=TaskLogger;Integrated Security=True"))
+                {
+                    using (SqlCommand cmd = new SqlCommand("deletetask", con))
+                    {
+
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@taskid", Taskid);
+
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+
+                    }
+
+                }
+                return RedirectToAction("Index", "View", new { area = "" });
+            }
+            return View();
+        }
+
     }
 }
