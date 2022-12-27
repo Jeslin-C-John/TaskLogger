d236a3cad0dc49b5a4ac2cbcbd267dad3eeba86b
Jeslin-C-John <cjohnjeslin@gmail.com>
2022-12-27 18:31:20 +0530

    Completed

--- TaskLogger/Controllers/UpdateController.cs
+++ TaskLogger/Controllers/UpdateController.cs
@@ -6,20 +6,49 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TaskLogger.Models;
+using System.Threading.Tasks;
 
 namespace TaskLogger.Controllers
 {
     public class UpdateController : Controller
     {
         [HttpGet]
-        public ActionResult Index()
+        public ActionResult Index(int Taskid)
         {
             ViewBag.Name = Session["Name"];
-            return View();
+            using (SqlConnection con = new SqlConnection("Data Source=HP_5300U;Initial Catalog=TaskLogger;Integrated Security=True"))
+            {
+
+                using (SqlCommand cmd = new SqlCommand("viewupdate", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@taskid", Taskid);
+
+
+                    con.Open();
+              
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    var instance = new TaskLogger.Models.Task();
+
+                    rdr.Read();
+                        
+                    instance.Date = rdr.GetDateTime(0);
+                    instance.Hours = rdr.GetInt32(1);
+                    instance.BoolStatus = rdr.GetBoolean(2);
+
+
+                    con.Close();
+
+                    return View(instance);
+                }
+
+            }
+
         }
 
         [HttpPost]
-        public ActionResult Index(Task instance,int Taskid)
+        public ActionResult Index(TaskLogger.Models.Task instance,int Taskid)
         {
             ViewBag.Name = Session["Name"];
             if (ModelState.IsValid)
